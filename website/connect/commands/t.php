b5ff94d6638ecb56d05a472b863a9b3e6068a505
= <=>
2016-08-04 21:14:36 +0100

    Changed folder name and deleted unused website folder

--- website/connect/commands/t.php
+++
@@ -1,5 +0,0 @@
-<?php
-$db = new SQLite3('data.db');
-$db->exec('DELETE FROM whoIsNotIn WHERE teacher="Bobby"');
-echo "Row deleted \n";
-?>
--- website/connect/logout.php
+++
@@ -1,6 +0,0 @@
-<?php 
-
-setcookie('login', null, -1, '/');
-header( 'Location: ../index.php' ) ; 
-
-?>
